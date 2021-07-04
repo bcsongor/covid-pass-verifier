f40193deffd400774b6b093d51bbdb10dc0bf7a4
Csongor Bokay <8850110+bcsongor@users.noreply.github.com>
2021-07-04 17:31:20 +0000

    Added deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+yarn build
+
+source .env
+export AZCOPY_SPA_CLIENT_SECRET=$AZ_AD_SP_CLIENT_SECRET
+azcopy login --service-principal --application-id $AZ_AD_SP_APP_ID --tenant-id $AZ_AD_SP_TENANT_ID
+azcopy copy "./build/*" "$AZ_BLOB_ENDPOINT/\$web" --recursive
